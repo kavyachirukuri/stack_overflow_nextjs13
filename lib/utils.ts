aff106d4f14f2dad32be976ed540082cdc7118b6
chirukuri kavya <chirukuri.kavya@credo.health>
2024-04-07 12:52:07 +0530

    question card commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,55 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const currentTime = new Date().getTime();
+  const createdAtTime = createdAt.getTime();
+  const timeDifference = Math.abs(currentTime - createdAtTime); // Time difference in milliseconds
+
+  // Define time units in milliseconds
+  const minute = 60 * 1000;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+  const week = 7 * day;
+  const month = 30 * day;
+  const year = 365 * day;
+
+  // Determine the appropriate time unit and calculate the time elapsed
+  if (timeDifference < minute) {
+    return "just now";
+  } else if (timeDifference < hour) {
+    const minutes = Math.floor(timeDifference / minute);
+    return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+  } else if (timeDifference < day) {
+    const hours = Math.floor(timeDifference / hour);
+    return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  } else if (timeDifference < week) {
+    const days = Math.floor(timeDifference / day);
+    return `${days} day${days > 1 ? "s" : ""} ago`;
+  } else if (timeDifference < month) {
+    const weeks = Math.floor(timeDifference / week);
+    return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
+  } else if (timeDifference < year) {
+    const months = Math.floor(timeDifference / month);
+    return `${months} month${months > 1 ? "s" : ""} ago`;
+  } else {
+    const years = Math.floor(timeDifference / year);
+    return `${years} year${years > 1 ? "s" : ""} ago`;
+  }
+};
+
+export const formatAndDivideNumber = (num: number): string => {
+  if (num >= 1000000) {
+    const formattedNum = (num / 1000000).toFixed(1);
+    return `${formattedNum}M`;
+  } else if (num >= 1000) {
+    const formattedNum = (num / 1000).toFixed(1);
+    return `${formattedNum}K`;
+  } else {
+    return num.toString();
+  }
+};
