fea0b79398cb2b38b486e86fa743f6d062694126
chirukuri kavya <chirukuri.kavya@credo.health>
2024-04-20 17:05:26 +0530

    add api/webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,17 @@ import { authMiddleware } from "@clerk/nextjs";
 // for more information about configuring your Middleware
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
