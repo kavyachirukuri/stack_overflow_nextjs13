ce72c53bc12e2cae1288f48904bf09f21c3a680f
chirukuri kavya <chirukuri.kavya@credo.health>
2024-06-10 15:26:43 +0530

    ...

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -34,7 +34,7 @@ const RightSidebar = async () => {
           ))}
         </div>
       </div>
-      <div className="mt-16">Popular Tags</div>
+      <h3 className="h3-bold text-dark200_light900 mt-16">Popular Tags</h3>{" "}
       <div className="mt-7 flex flex-col gap-4">
         {popularTags.map((tag) => (
           <RenderTag
